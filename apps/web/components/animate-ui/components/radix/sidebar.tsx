5ac881a21a1f25dce2c79fc829d53179f9335e8e
Dasun Wickramasooriya <dasun.wickramasooriya@gmail.com>
2026-02-21 21:03:20 +0530

    refactor(web): sidebar update

--- apps/web/components/animate-ui/components/radix/sidebar.tsx
+++ apps/web/components/animate-ui/components/radix/sidebar.tsx
@@ -255,7 +255,7 @@ function Sidebar({
       <div
         data-slot="sidebar-container"
         className={cn(
-          "fixed inset-y-0 top-4 z-10 hidden h-[calc(100vh-1rem)] w-(--sidebar-width) transition-[left,right,width] duration-400 ease-[cubic-bezier(0.75,0,0.25,1)] md:flex",
+          "fixed top-4 bottom-0 z-10 hidden h-[calc(100vh-1rem)] w-(--sidebar-width) transition-[left,right,width] duration-400 ease-[cubic-bezier(0.75,0,0.25,1)] md:flex",
           side === "left"
             ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
             : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
